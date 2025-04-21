9798b8ed00500632e1da84b9ffd517df475cea91
dap3884 <dap3@iu.edu>
2025-04-21 15:03:07 -0400

    Modify the code

--- src/main/java/edu/iu/habahram/GumballMachine/model/GumballMachine.java
+++ src/main/java/edu/iu/habahram/GumballMachine/model/GumballMachine.java
@@ -42,8 +42,12 @@ public class GumballMachine implements IGumballMachine {
             message = "You ejected a quarter";
             succeeded = true;
             state = NO_QUARTER;
-        } else {
+        } else if(state.equalsIgnoreCase(SOLD)){
             message = "You can't eject a quarter, the machine has no quarter";
+        } else if(state.equalsIgnoreCase(SOLD_OUT)){
+            message = "You can't eject a quarter, the machine has no gumball";
+        } else{
+            message = "First, you have to insert a quarter in order to eject a quarter";
         }
         return new TransitionResult(succeeded, message, state, count);
     }
@@ -54,11 +58,14 @@ public class GumballMachine implements IGumballMachine {
         String message = "";
 
         if (state.equalsIgnoreCase(HAS_QUARTER)) {
-            message = "Please wait, we're dispensing a gumball";
-            succeeded = true;
             state = SOLD;
-        } else {
-            message = "You can't dispense a gumball, the machine has no quarter";
+            return this.dispense();
+        } else if(state.equalsIgnoreCase(SOLD)){
+            message = "You have to pay first to get a gumball";
+        } else if(state.equalsIgnoreCase(SOLD_OUT)){
+            message = "You can't get gumball, the machine has no gumball";
+        } else{
+            message = "First, you have to insert a quarter in order to get a gumball";
         }
         return new TransitionResult(succeeded, message, state, count);
     }
@@ -79,8 +86,22 @@ public class GumballMachine implements IGumballMachine {
     }
 
     @Override
-    public void releaseBall() {
+    public TransitionResult dispense() {
+        boolean succeeded = false;
+        String message = "";
 
+        if (state.equalsIgnoreCase(HAS_QUARTER)) {
+            message = "First, you have to crank gumball machine";
+        } else if (state.equalsIgnoreCase(NO_QUARTER)) {
+            message = "You have to pay first";
+        } else if (state.equalsIgnoreCase(SOLD_OUT)) {
+            message = "There is no gumball in the machine";
+        } else if (state.equalsIgnoreCase(SOLD)) {
+            message = "We're dispensing a gumball";
+            this.count -= 1;
+            succeeded = true;
+        }
+        return new TransitionResult(succeeded, message, state, count);
     }
 
 
